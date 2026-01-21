3b64bce27824aa9cd524210bda61b961d844dcf3
akwloo <akwloo@gmail.com>
2026-01-21 11:25:21 +0800

    update erb8 import-export branch

--- crm/resources.py
+++ crm/resources.py
@@ -3,21 +3,21 @@ from .models import Employee
 
 class EmployeeResource(resources.ModelResource):
 
-    def before_import(self, queryset, **kwargs):
-        Employee.objects.all().delete()
-        super().before_import(queryset, **kwargs)
+    #def before_import(self, queryset, **kwargs):
+        #Employee.objects.all().delete()
+        #super().before_import(queryset, **kwargs)
 
     def before_import_row(self, row, **kwargs):
-        row["first_name"] = f"{str(row['first_name']):.{10}}"
-        row["last_name"] = f"{str(row['last_name']):.{10}}"
+        row["first_name"] = f"{str(row['first_name']).strip():.{10}}"
+        row["last_name"] = f"{str(row['last_name']).strip():.{10}}"
         row["age"] = f"{row['age']:.{5}}"
         row["monthly_salary"] = f"{float(row['monthly_salary']):.{10}}"
-        row["join_date"] = f"{row['join_date']:.{15}}"
+        row["join_date"] = f"{str(row['join_date']).strip():.{15}}"
         super().before_import_row(row, **kwargs)
 
-    def after_export(self, queryset, data, **kwargs):
-        Employee.objects.all().delete()
-        super().after_export(queryset, data, **kwargs)
+    #def after_export(self, queryset, data, **kwargs):
+        #Employee.objects.all().delete()
+        #super().after_export(queryset, data, **kwargs)
 
     class Meta:
         model = Employee
--- product/resources.py
+++ product/resources.py
@@ -3,32 +3,32 @@ from .models import Product
 
 class ProductResource(resources.ModelResource):
 
-    def before_import(self, queryset, **kwargs):
-        Product.objects.all().delete()
-        super().before_import(queryset, **kwargs)
+    #def before_import(self, queryset, **kwargs):
+        #Product.objects.all().delete()
+        #super().before_import(queryset, **kwargs)
 
     def before_import_row(self, row, **kwargs):
-        row["title"] = f"{str(row['title']):.{50}}"
-        row["description"] = f"{str(row['description']):.{250}}"
-        row["category"] = f"{str(row['category']):.{20}}"
+        row["title"] = f"{str(row['title']).strip():.{50}}"
+        row["description"] = f"{str(row['description']).strip():.{250}}"
+        row["category"] = f"{str(row['category']).strip():.{20}}"
         row["price"] = f"{float(row['price']):.{10}}"
         row["discountPercentage"] = f"{float(row['discountPercentage']):.{6}}"
         row["rating"] = f"{float(row['rating']):.{6}}"
         row["stock"] = f"{float(row['stock']):.{5}}"
-        row["brand"] = f"{str(row['brand']):.{15}}"
-        row["sku"] = f"{str(row['sku']):.{20}}"
+        row["brand"] = f"{str(row['brand']).strip():.{15}}"
+        row["sku"] = f"{str(row['sku']).strip():.{20}}"
         row["weight"] = f"{float(row['weight']):.{5}}"
-        row["warrantyInformation"] = f"{str(row['warrantyInformation']):.{25}}"
-        row["shippingInformation"] = f"{str(row['shippingInformation']):.{50}}"
-        row["availabilityStatus"] = f"{str(row['availabilityStatus']):.{10}}"
-        row["returnPolicy"] = f"{str(row['returnPolicy']):.{25}}"
+        row["warrantyInformation"] = f"{str(row['warrantyInformation']).strip():.{25}}"
+        row["shippingInformation"] = f"{str(row['shippingInformation']).strip():.{50}}"
+        row["availabilityStatus"] = f"{str(row['availabilityStatus']).strip():.{10}}"
+        row["returnPolicy"] = f"{str(row['returnPolicy']).strip():.{25}}"
         row["minimumOrderQuantity"] = f"{float(row['minimumOrderQuantity']):.{5}}"
-        row["thumbnail"] = f"{str(row['thumbnail']):.{200}}"
+        row["thumbnail"] = f"{str(row['thumbnail']).strip():.{200}}"
         super().before_import_row(row, **kwargs)
 
-    def after_export(self, queryset, data, **kwargs):
-        Product.objects.all().delete()
-        super().after_export(queryset, data, **kwargs)
+    #def after_export(self, queryset, data, **kwargs):
+        #Product.objects.all().delete()
+        #super().after_export(queryset, data, **kwargs)
 
     class Meta:
         model = Product
